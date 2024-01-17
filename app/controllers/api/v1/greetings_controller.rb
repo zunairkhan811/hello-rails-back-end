5b13af2247829d2cfd92502f17cb495c23c3cd67
Muhammad Zunair khan <zunairkhan811@gmail.com>
2024-01-17 11:55:35 +0500

    delete files

--- app/controllers/api/v1/greetings_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::GreetingsController < ApplicationController
-  def index
-    @random_greeting = Greeting.order('RANDOM()').first
-    render json: @random_greeting, status: :ok
-  end
-end
